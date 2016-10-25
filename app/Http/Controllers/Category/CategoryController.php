9e0554c22a76bbfaee7a34c61ac6c2fb49174039
Juan Benjumea <juanbenjumea@gmail.com>
2016-10-25 18:39:26 -0500

    Consultar listado de deportes, categoría e info de categoría

---
+++ app/Http/Controllers/Category/CategoryController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Custom\Http\Controllers\Sport;
+
+use Custom\Http\Controllers\ApiController;
+use Custom\Utils\Transformers\SportTransformer;
+use Illuminate\Http\Request;
+use Custom\Models\Product\Sport;
+
+class SportController extends ApiController {
+
+    protected $sportTransformer;
+
+    public function __construct(SportTransformer $sportTransformer)
+    {
+        $this->sportTransformer = $sportTransformer;
+    }
+
+    public function index()
+    {
+        $sports = Sport::with(['translation', 'image', 'icon'])->get();
+
+        return $this->respond([
+            'data' => $this->sportTransformer->transformCollection($sports->all())
+        ]);
+    }
+
+    public function show($id)
+    {
+        $sport = Sport::with('translation', 'image', 'icon')->where('id', $id)->get();
+
+        return $this->respond([
+            'data' => $this->sportTransformer->transform($sport->first())
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        Sport::create($request->all());
+    }
+
+    public function update(Request $request, $id)
+    {
+        Sport::find($id)->update($request->all());
+    }
+
+    public function destroy($id)
+    {
+        Sport::find($id)->delete();
+    }
+}
